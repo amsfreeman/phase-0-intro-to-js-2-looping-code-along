39920ad6c2f1283d5e5c53c685a6ac6ecef01f7d
Amelia Freeman <ameliakfreeman@gmail.com>
2023-05-11 09:56:04 -0500

    Passed Looping Tests

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+const newArray = [];
+
+function writeCards(arrayOfNames, stringOfEvent) {
+    for (let i = 0; i < arrayOfNames.length; i++) {
+    newArray.push(`Thank you, ${arrayOfNames[i]}, for the wonderful ${stringOfEvent} gift!`);
+    };
+    return newArray;
+};
+writeCards(arrayOfNames, stringofEvent);
+
+function countDown() {
+    let number = 10;
+    while (number >= 0){
+        console.log(number--);
+    }
+}
